d567dca20f7d74f2bbe43add83ae90a3bc06fced
tubi55 <tubi55@nate.com>
2022-06-07 14:39:22 +0900

    전환효과 모션 추가

--- src/componetns/common/Layout.js
+++ src/componetns/common/Layout.js
@@ -1,6 +1,14 @@
+import { useEffect, useRef } from 'react';
+
 function Layout({ name }) {
+	const frame = useRef(null);
+
+	useEffect(() => {
+		frame.current.classList.add('on');
+	}, []);
+
 	return (
-		<section className={`content ${name}`}>
+		<section className={`content ${name}`} ref={frame}>
 			<figure></figure>
 			<div className='inner'>
 				<h2>{name}</h2>
