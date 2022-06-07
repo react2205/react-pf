31875f8d322755618b93087f65b73b49fd20a6a3
tubi55 <tubi55@nate.com>
2022-06-07 11:32:19 +0900

    기본 header 스타일링

--- src/componetns/common/Header.js
+++ src/componetns/common/Header.js
@@ -1,47 +1,48 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 function Header() {
 	const style = { color: 'hotpink' };
+
 	return (
 		<header>
-			<h1>
-				<NavLink activeStyle={style} to='/'>
-					DCODELAB
-				</NavLink>
-			</h1>
+			<div className='inner'>
+				<h1>
+					<Link to='/'>DCODELAB</Link>
+				</h1>
 
-			<ul id='gnb'>
-				<li>
-					<NavLink activeStyle={style} to='/department'>
-						DEPARTMENT
-					</NavLink>
-				</li>
-				<li>
-					<NavLink activeStyle={style} to='/community'>
-						COMMUNITY
-					</NavLink>
-				</li>
-				<li>
-					<NavLink activeStyle={style} to='/gallery'>
-						GALLERY
-					</NavLink>
-				</li>
-				<li>
-					<NavLink activeStyle={style} to='/youtube'>
-						YOUTUBE
-					</NavLink>
-				</li>
-				<li>
-					<NavLink activeStyle={style} to='/location'>
-						LOCATION
-					</NavLink>
-				</li>
-				<li>
-					<NavLink activeStyle={style} to='/join'>
-						JOIN
-					</NavLink>
-				</li>
-			</ul>
+				<ul id='gnb'>
+					<li>
+						<NavLink activeStyle={style} to='/department'>
+							DEPARTMENT
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={style} to='/community'>
+							COMMUNITY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={style} to='/gallery'>
+							GALLERY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={style} to='/youtube'>
+							YOUTUBE
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={style} to='/location'>
+							LOCATION
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={style} to='/join'>
+							JOIN
+						</NavLink>
+					</li>
+				</ul>
+			</div>
 		</header>
 	);
 }
