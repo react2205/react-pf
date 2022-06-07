d868bb83429c6c92ba87b2fb2e732ab1760fcf18
tubi55 <tubi55@nate.com>
2022-06-07 10:50:59 +0900

    각 페이지별 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
+import { Route } from 'react-router-dom';
+import Header from './componetns/common/Header';
+import Footer from './componetns/common/Footer';
+//main
+import Visual from './componetns/main/Visual';
+import News from './componetns/main/News';
+import Pics from './componetns/main/Pics';
+import Vids from './componetns/main/Vids';
+//sub
+import Department from './componetns/sub/Department';
+import Gallery from './componetns/sub/Gallery';
+import Community from './componetns/sub/Community';
+import Youtube from './componetns/sub/Youtube';
+import Location from './componetns/sub/Location';
+import Join from './componetns/sub/Join';
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<Header />
+
+			<Route exact path='/'>
+				<Visual />
+				<News />
+				<Pics />
+				<Vids />
+			</Route>
+
+			<Route path='/department' component={Department} />
+			<Route path='/community' component={Community} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/location' component={Location} />
+			<Route path='/join' component={Join} />
+
+			<Footer />
+		</>
 	);
 }
 
