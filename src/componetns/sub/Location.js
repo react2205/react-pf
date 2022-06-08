c0c878a3a475ec842a417f79183ea10358ed6a5b
tubi55 <tubi55@nate.com>
2022-06-08 11:26:58 +0900

    지도에 마커 출력

--- src/componetns/sub/Location.js
+++ src/componetns/sub/Location.js
@@ -2,6 +2,7 @@ import Layout from '../common/Layout';
 import { useEffect, useRef } from 'react';
 
 function Location() {
+	//윈도우 전역객체에 있는 kakao키값을 바로 변수로 비구조화 할당
 	const { kakao } = window;
 	const container = useRef(null);
 	const option = {
@@ -9,8 +10,26 @@ function Location() {
 		level: 3,
 	};
 
+	//위치 인스턴스 생성
+	const markerPosition = new kakao.maps.LatLng(
+		33.450701,
+		126.570667
+	);
+
+	//위치 인스턴스 값을 인수로 해서 마커 인스턴스 생성
+	const marker = new kakao.maps.Marker({
+		position: markerPosition,
+	});
+	//최종 마커 호출
+
 	useEffect(() => {
-		new kakao.maps.Map(container.current, option);
+		//지도 인스턴스 생성
+		const map_instance = new kakao.maps.Map(
+			container.current,
+			option
+		);
+		//마커 출력
+		marker.setMap(map_instance);
 	}, []);
 
 	return (
