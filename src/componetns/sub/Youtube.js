0109cf2956eb1d44bab772bf6bdcd410ba4fe887
tubi55 <tubi55@nate.com>
2022-06-08 10:37:59 +0900

    팝업에 유튜브 영상 호출

--- src/componetns/sub/Youtube.js
+++ src/componetns/sub/Youtube.js
@@ -6,6 +6,7 @@ import Popup from '../common/Popup';
 function Youtube() {
 	const [Vids, setVids] = useState([]);
 	const [Open, setOpen] = useState(false);
+	const [Index, setIndex] = useState(0);
 
 	useEffect(() => {
 		const key = 'AIzaSyC77Pd__ju0Wqx_Umc-IuW7Cn2mWi_HVsk';
@@ -44,7 +45,10 @@ function Youtube() {
 							</div>
 							<div
 								className='pic'
-								onClick={() => setOpen(true)}>
+								onClick={() => {
+									setOpen(true);
+									setIndex(idx);
+								}}>
 								<img
 									src={vid.snippet.thumbnails.standard.url}
 									alt={vid.title}
@@ -54,7 +58,13 @@ function Youtube() {
 					);
 				})}
 			</Layout>
-			{Open ? <Popup setOpen={setOpen} /> : null}
+			{Open && (
+				<Popup setOpen={setOpen}>
+					<iframe
+						src={`https://www.youtube.com/embed/${Vids[Index].snippet.resourceId.videoId}`}
+						frameBorder='0'></iframe>
+				</Popup>
+			)}
 		</>
 	);
 }
