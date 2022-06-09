56e98279714c25ee5fce9f3a959b1cea044902c2
tubi55 <tubi55@nate.com>
2022-06-09 09:36:16 +0900

    인풋요소에 change이벤트 발생할 시 해당 값을 state에 저장하고 출력

--- src/componetns/sub/Join.js
+++ src/componetns/sub/Join.js
@@ -1,9 +1,45 @@
 import Layout from '../common/Layout';
+import { useState } from 'react';
 
 function Join() {
+	const initVal = {
+		userid: '',
+	};
+	const [Val, setVal] = useState(initVal);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		console.log(`name:${name}/ value:${value}`);
+		setVal({ ...Val, [name]: value });
+	};
+
 	return (
 		<Layout name={'Join'}>
-			<p>조인 컨텐츠 페이지 입니다.</p>
+			<form>
+				<fieldset>
+					<legend>회원가입 폼 양식</legend>
+					<table border='1'>
+						<caption>회원가입 정보입력</caption>
+						<tbody>
+							<tr>
+								<th scope='row'>
+									<label htmlFor='userid'>USER ID</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='userid'
+										name='userid'
+										placeholder='아이디를 입력하세요.'
+										value={Val.userid}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
 		</Layout>
 	);
 }
