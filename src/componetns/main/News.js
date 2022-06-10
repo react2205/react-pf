502660afd2f1b9e976c1e2adbd6bf2c50db02999
tubi55 <tubi55@nate.com>
2022-06-10 12:28:09 +0900

    메인페이지 최신글 미리보기

--- src/componetns/main/News.js
+++ src/componetns/main/News.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import { faArrowAltCircleLeft } from '@fortawesome/free-regular-svg-icons';
+import { useState, useEffect } from 'react';
 
 function News() {
-	return <section id='news'>News</section>;
+	//로컬저장소에서 데이터를 받아와서 json형태로 반환
+	const getLocalData = () => {
+		const data = localStorage.getItem('post');
+		const dummyPosts = [
+			{ title: 'Hello5', content: 'Here comes description in detail.' },
+			{ title: 'Hello4', content: 'Here comes description in detail.' },
+			{ title: 'Hello3', content: 'Here comes description in detail.' },
+			{ title: 'Hello2', content: 'Here comes description in detail.' },
+			{ title: 'Hello1', content: 'Here comes description in detail.' },
+		];
+
+		if (data) {
+			return JSON.parse(data);
+		} else {
+			return dummyPosts;
+		}
+	};
+
+	const [Posts] = useState(getLocalData());
+
+	useEffect(() => {
+		localStorage.setItem('post', JSON.stringify(Posts));
+	}, []);
+	return (
+		<section id='news'>
+			{Posts.map((post, idx) => {
+				if (idx < 4) {
+					return (
+						<article key={idx}>
+							<h2>{post.title}</h2>
+							<p>{post.content}</p>
+						</article>
+					);
+				}
+			})}
+		</section>
+	);
 }
 
 export default News;
--- src/componetns/sub/Community.js
+++ src/componetns/sub/Community.js
@@ -7,22 +7,9 @@ function Community() {
 	const inputEdit = useRef(null);
 	const textareaEdit = useRef(null);
 
-	//로컬저장소에서 데이터를 받아와서 json형태로 반환
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
-		const dummyPosts = [
-			{ title: 'Hello5', content: 'Here comes description in detail.' },
-			{ title: 'Hello4', content: 'Here comes description in detail.' },
-			{ title: 'Hello3', content: 'Here comes description in detail.' },
-			{ title: 'Hello2', content: 'Here comes description in detail.' },
-			{ title: 'Hello1', content: 'Here comes description in detail.' },
-		];
-
-		if (data) {
-			return JSON.parse(data);
-		} else {
-			return dummyPosts;
-		}
+		return JSON.parse(data);
 	};
 
 	const [Posts, setPosts] = useState(getLocalData());
