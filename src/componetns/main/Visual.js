ef5653cb12da1f97dec371e3203173df8b575df4
tubi55 <tubi55@nate.com>
2022-06-10 12:48:27 +0900

    Anime클래스 활용

--- src/componetns/main/Visual.js
+++ src/componetns/main/Visual.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import Anime from '../../asset/anim.js';
+import { useRef } from 'react';
 
 function Visual() {
-	return <figure id='visual'>Visual</figure>;
+	const box = useRef(null);
+	const style = {
+		width: 100,
+		height: 100,
+		backgroundColor: 'aqua',
+		position: 'absolute',
+		top: 200,
+		left: 0,
+	};
+	return (
+		<figure id='visual'>
+			<div
+				ref={box}
+				className='box'
+				style={style}
+				onClick={() => {
+					new Anime(window, {
+						prop: 'scroll',
+						value: 1000,
+						duration: 500,
+					});
+				}}></div>
+		</figure>
+	);
 }
 
 export default Visual;
