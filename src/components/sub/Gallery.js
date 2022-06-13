366cd5cc8d953cb9f4888d2d49c46dafc2f7591c
tubi55 <tubi55@nate.com>
2022-06-13 11:46:34 +0900

    기본 데이터 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,9 +1,42 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useState, useEffect, useRef } from 'react';
 
 function Gallery() {
+	const frame = useRef(null);
+	const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+	const method_interest = 'flickr.interestingness.getList';
+	const num = 20;
+	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${num}&nojsoncallback=1&format=json`;
+
+	const [Items, setItems] = useState([]);
+
+	useEffect(() => {
+		axios.get(url).then((json) => {
+			console.log(json.data.photos.photo);
+			setItems(json.data.photos.photo);
+		});
+	}, []);
+
 	return (
 		<Layout name={'Gallery'}>
-			<p>갤러리 컨텐츠 페이지 입니다.</p>
+			<div className='frame' ref={frame}>
+				{Items.map((item, idx) => {
+					return (
+						<article key={idx}>
+							<div className='inner'>
+								<div className='pic'>
+									<img
+										src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+										alt=''
+									/>
+								</div>
+								<h2>{item.title}</h2>
+							</div>
+						</article>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
