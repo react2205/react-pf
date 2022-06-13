c980118032eaad876c350812a8e58f19a79d490b
tubi55 <tubi55@nate.com>
2022-06-13 12:13:00 +0900

    모든 데이터가 호출되면 화면 출력

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -4,18 +4,24 @@ import { useState, useEffect, useRef } from 'react';
 
 function Gallery() {
 	const frame = useRef(null);
-	const key = '4612601b324a2fe5a1f5f7402bf8d87a';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 20;
-	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${num}&nojsoncallback=1&format=json`;
 
 	const [Items, setItems] = useState([]);
 
-	useEffect(() => {
-		axios.get(url).then((json) => {
-			console.log(json.data.photos.photo);
+	const getFlickr = async () => {
+		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 500;
+		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${num}&nojsoncallback=1&format=json`;
+
+		await axios.get(url).then((json) => {
 			setItems(json.data.photos.photo);
 		});
+
+		frame.current.classList.add('on');
+	};
+
+	useEffect(() => {
+		getFlickr();
 	}, []);
 
 	return (
