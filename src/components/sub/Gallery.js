94b19bd25a9017c00f2379a0f13598ee21e2c017
tubi55 <tubi55@nate.com>
2022-06-13 12:22:17 +0900

    갤러리 전환

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -4,15 +4,16 @@ import { useState, useEffect, useRef } from 'react';
 
 function Gallery() {
 	const frame = useRef(null);
-
 	const [Items, setItems] = useState([]);
 
-	const getFlickr = async () => {
-		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
-		const method_interest = 'flickr.interestingness.getList';
-		const num = 500;
-		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${num}&nojsoncallback=1&format=json`;
+	const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+	const method_interest = 'flickr.interestingness.getList';
+	const method_search = 'flickr.photos.search';
+	const num = 500;
+	const interest_url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${num}&nojsoncallback=1&format=json`;
+	const search_url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${key}&per_page=${num}&nojsoncallback=1&format=json&tags=ocean`;
 
+	const getFlickr = async (url) => {
 		await axios.get(url).then((json) => {
 			setItems(json.data.photos.photo);
 		});
@@ -21,11 +22,26 @@ function Gallery() {
 	};
 
 	useEffect(() => {
-		getFlickr();
+		getFlickr(interest_url);
 	}, []);
 
 	return (
 		<Layout name={'Gallery'}>
+			<button
+				onClick={() => {
+					frame.current.classList.remove('on');
+					getFlickr(interest_url);
+				}}>
+				Interest Gallery
+			</button>
+			<button
+				onClick={() => {
+					frame.current.classList.remove('on');
+					getFlickr(search_url);
+				}}>
+				Search Gallery
+			</button>
+
 			<div className='frame' ref={frame}>
 				{Items.map((item, idx) => {
 					return (
