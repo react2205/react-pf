15cbad5c8a2d6238dd7bc0f24680c794cee6e54e
tubi55 <tubi55@nate.com>
2022-06-13 10:57:50 +0900

    swiper 커스텀

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,20 +1,38 @@
-import { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 function Visual() {
 	return (
 		<figure id='visual' className='myScroll'>
-			<Swiper>
-				<SwiperSlide>Slide 1</SwiperSlide>
-				<SwiperSlide>Slide 2</SwiperSlide>
-				<SwiperSlide>Slide 3</SwiperSlide>
-				<SwiperSlide>Slide 4</SwiperSlide>
-				<SwiperSlide>Slide 5</SwiperSlide>
-				<SwiperSlide>Slide 6</SwiperSlide>
-				<SwiperSlide>Slide 7</SwiperSlide>
-				<SwiperSlide>Slide 8</SwiperSlide>
-				<SwiperSlide>Slide 9</SwiperSlide>
+			<Swiper
+				navigation={true}
+				modules={[Navigation, Pagination]}
+				pagination={{ clickable: true }}
+				spaceBetween={50}
+				loop={true}
+				slidesPerView={3}
+				centeredSlides={true}>
+				<SwiperSlide>
+					<div className='inner'>1</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className='inner'>2</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className='inner'>3</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className='inner'>4</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className='inner'>5</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className='inner'>6</div>
+				</SwiperSlide>
 			</Swiper>
 		</figure>
 	);
