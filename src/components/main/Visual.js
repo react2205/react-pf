b153f7ce8bbd680b2f2169a30f1ccda4e43cc841
tubi55 <tubi55@nate.com>
2022-06-13 10:30:36 +0900

    start값이 있을때에만 pic에서 start값을 활용해 위치값 보정

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,3 +1,7 @@
+import { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
 function Visual() {
 	return <figure id='visual' className='myScroll'></figure>;
 }
