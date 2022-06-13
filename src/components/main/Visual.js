b9e1e73c9b73a6e99f1ec95868e6b1fa6b14e0d8
tubi55 <tubi55@nate.com>
2022-06-13 10:36:30 +0900

    기본 swiper slide 적용

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -3,7 +3,21 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 function Visual() {
-	return <figure id='visual' className='myScroll'></figure>;
+	return (
+		<figure id='visual' className='myScroll'>
+			<Swiper>
+				<SwiperSlide>Slide 1</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+				<SwiperSlide>Slide 5</SwiperSlide>
+				<SwiperSlide>Slide 6</SwiperSlide>
+				<SwiperSlide>Slide 7</SwiperSlide>
+				<SwiperSlide>Slide 8</SwiperSlide>
+				<SwiperSlide>Slide 9</SwiperSlide>
+			</Swiper>
+		</figure>
+	);
 }
 
 export default Visual;
