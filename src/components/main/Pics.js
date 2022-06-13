8e2891b6c29ce737e8deb0ebfddaec715345dd68
tubi55 <tubi55@nate.com>
2022-06-13 10:12:46 +0900

    시간차 적용

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -3,7 +3,8 @@ function Pics({ Scrolled, start, base }) {
 
 	return (
 		<section id='pic' className='myScroll'>
-			<p style={position >= 0 ? { left: 100 + position } : null}>FLICKR</p>
+			<p style={{ left: 100 + position }}>FLICKR</p>
+			<h3 style={{ left: 100 + position / 2 }}>FLICKR</h3>
 		</section>
 	);
 }
