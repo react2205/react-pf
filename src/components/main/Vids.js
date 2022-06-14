4078280a69cabbde3bf1fbd0eee5407550940211
tubi55 <tubi55@nate.com>
2022-06-14 15:45:36 +0900

    메인페이지 유튜브 미리보기 팝업연결

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,25 +1,46 @@
 import { useSelector } from 'react-redux';
+import { useRef, useState } from 'react';
+import Popup from '../common/Popup';
 
 function Vids() {
 	const Vids = useSelector((store) => store.youtubeReducer.youtube);
+	const [Index, setIndex] = useState(0);
+	const pop = useRef(null);
+
 	return (
-		<section id='vids' className='myScroll'>
-			{Vids.map((vid, idx) => {
-				if (idx < 4) {
-					return (
-						<article key={idx}>
-							<div className='pic'>
-								<img
-									src={vid.snippet.thumbnails.standard.url}
-									alt={vid.snippet.title}
-								/>
-							</div>
-							<p>{vid.snippet.title}</p>
-						</article>
-					);
-				}
-			})}
-		</section>
+		<>
+			<section id='vids' className='myScroll'>
+				{Vids.map((vid, idx) => {
+					if (idx < 4) {
+						return (
+							<article key={idx}>
+								<div
+									className='pic'
+									onClick={() => {
+										setIndex(idx);
+										pop.current.open();
+									}}>
+									<img
+										src={vid.snippet.thumbnails.standard.url}
+										alt={vid.snippet.title}
+									/>
+								</div>
+							</article>
+						);
+					}
+				})}
+			</section>
+
+			<Popup ref={pop}>
+				<>
+					{Vids.length !== 0 && (
+						<iframe
+							src={`https://www.youtube.com/embed/${Vids[Index].snippet.resourceId.videoId}`}
+							frameBorder='0'></iframe>
+					)}
+				</>
+			</Popup>
+		</>
 	);
 }
 
