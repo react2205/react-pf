9c93daaf2fc84a750aba9122861427cf753653c3
tubi55 <tubi55@nate.com>
2022-06-15 11:18:48 +0900

    컴포넌트에 리턴될 JSX추가

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -3,7 +3,69 @@ import Layout from '../common/Layout';
 function Flickr() {
 	return (
 		<>
-			<Layout name={'Flickr'}></Layout>
+			<Layout name={'Flickr'}>
+				{Loading && (
+					<img
+						className='loading'
+						src={`${process.env.PUBLIC_URL}/img/loading.gif`}
+					/>
+				)}
+				<button>Interest Gallery</button>
+				<div className='searchBox'>
+					<input
+						type='text'
+						ref={input}
+						placeholder='검색어를 입력하세요'
+						onKeyUp={(e) => {
+							if (e.key === 'Enter') showSearch();
+						}}
+					/>
+					<button onClick={showSearch}>SEARCH</button>
+				</div>
+
+				<div className='frame' ref={frame}>
+					<Masonry elementType={'div'} options={masonryOptions}>
+						{Items.map((item, idx) => {
+							return (
+								<article key={idx}>
+									<div className='inner'>
+										<div className='pic'>
+											<img
+												src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+												alt={item.title}
+											/>
+										</div>
+										<h2>{item.title}</h2>
+										<div className='profile'>
+											<img
+												src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+												alt={item.owner}
+												onError={(e) => {
+													e.target.setAttribute(
+														'src',
+														'https://www.flickr.com/images/buddyicon.gif'
+													);
+												}}
+											/>
+											<span>{item.owner}</span>
+										</div>
+									</div>
+								</article>
+							);
+						})}
+					</Masonry>
+				</div>
+			</Layout>
+
+			{/* 컴포넌트자체를 useRef로 참조 */}
+			<Popup ref={pop}>
+				{Items.length !== 0 && (
+					<img
+						src={`https://live.staticflickr.com/${Items[Index].server}/${Items[Index].id}_${Items[Index].secret}_b.jpg`}
+						alt={Items[Index].title}
+					/>
+				)}
+			</Popup>
 		</>
 	);
 }
