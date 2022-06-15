9eca2c10fcb0e5da1436577b355293261efa2279
tubi55 <tubi55@nate.com>
2022-06-15 14:22:21 +0900

    검색, 인터레스트, 유저이미지 기능 추가

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -12,6 +12,7 @@ function Flickr() {
 	const pop = useRef(null);
 	const [Index, setIndex] = useState(0);
 	const [Loading, setLoading] = useState(true);
+	const [EnableClick, setEnableClick] = useState(true);
 	//Opt값에 처음 api인수로 전달될 값으로 초기화
 	const [Opt, setOpt] = useState({
 		type: 'user',
@@ -24,7 +25,35 @@ function Flickr() {
 		setTimeout(() => {
 			frame.current.classList.add('on');
 			setLoading(false);
+			setTimeout(() => setEnableClick(true), 1000);
 		}, 1000);
+	};
+
+	const showInterest = () => {
+		if (!EnableClick) return;
+		setLoading(true);
+		frame.current.classList.remove('on');
+		setOpt({ type: 'interest', count: 50 });
+	};
+
+	const showSearch = () => {
+		if (!EnableClick) return;
+		const tag = input.current.value.trim();
+		input.current.value = '';
+		if (!tag) return alert('검색어를 입력하세요');
+
+		setLoading(true);
+		frame.current.classList.remove('on');
+		setOpt({ type: 'search', count: 50, tags: tag });
+	};
+
+	const showUser = (e) => {
+		if (!EnableClick) return;
+		const user = e.target.innerText;
+
+		setLoading(true);
+		frame.current.classList.remove('on');
+		setOpt({ type: 'user', count: 50, user: user });
 	};
 
 	//Opt값이 변경될때마다 해당 값을 FLICKR_START타입의 액션객체에 담아서 saga.js로 전달
@@ -41,6 +70,18 @@ function Flickr() {
 	return (
 		<>
 			<Layout name={'Flickr'}>
+				<button onClick={showInterest}>Show Interest</button>
+				<div className='searchBox'>
+					<input
+						type='text'
+						ref={input}
+						placeholder='검색어를 입력하세요'
+						onKeyUp={(e) => {
+							if (e.key === 'Enter') showSearch();
+						}}
+					/>
+					<button onClick={showSearch}>SEARCH</button>
+				</div>
 				{Loading && (
 					<img
 						className='loading'
@@ -71,7 +112,7 @@ function Flickr() {
 													);
 												}}
 											/>
-											<span>{item.owner}</span>
+											<span onClick={showUser}>{item.owner}</span>
 										</div>
 									</div>
 								</article>
